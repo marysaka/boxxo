6bb2a156f71066381272c09464d80a22b4a5a938
Mary Guillemard <mary@mary.zone>
2023-11-25 13:18:12 +0100

    Add basic boilerplate for Peripherals stuffs
    Signed-off-by: Mary Guillemard <mary@mary.zone>

--- boxxo-hal/src/lib.rs
+++ boxxo-hal/src/lib.rs
@@ -1,1 +1,28 @@
 #![no_std]
+
+pub use boxxo as pac;
+
+pub mod init_state {
+    pub struct Enabled<T = ()>(pub T);
+
+    pub struct Disabled;
+}
+
+#[allow(non_snake_case)]
+pub struct Peripherals {
+    // TODO: everything
+}
+
+impl Peripherals {
+    pub fn take() -> Option<Self> {
+        Some(Self::new(pac::Peripherals::take()?))
+    }
+
+    pub unsafe fn steal() -> Self {
+        Self::new(pac::Peripherals::steal())
+    }
+
+    fn new(_p: pac::Peripherals) -> Self {
+        Self {}
+    }
+}
